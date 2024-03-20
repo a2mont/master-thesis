ad91b1540a0e64f819085f057aa2443d4ec6dec4
a2mont <antoine.demont@outlook.fr>
2024-03-20 16:19:24 +0100

    update logs

--- Plugin-Master/Experiments3D.cc
+++ Plugin-Master/Experiments3D.cc
@@ -5,7 +5,7 @@ void Experiment3D::generate_torsion_mesh(double torsion_turns_count, bool _withR
 //    TetrahedralMesh mesh;
 //    TetrahedralizedVoxelGridGenerator<TetrahedralMesh>::generate_mesh(10, 10, 10, mesh);
     double target_angle = torsion_turns_count * 2 * M_PI;
-    double boundaryOnly(true);
+    double boundaryOnly(false);
 
     Eigen::Vector3d min_pos(std::numeric_limits<double>::max(), std::numeric_limits<double>::max(), std::numeric_limits<double>::max());
     Eigen::Vector3d max_pos(0.0, 0.0, 0.0);
--- Plugin-Master/logs/display_logs.py
+++ Plugin-Master/logs/display_logs.py
@@ -30,7 +30,7 @@ def extract_data(files: list, drop_zeros: bool = False, dimension='3D'):
             last_row = np.zeros(len(first_line))
             for row in reader:
                 [datas[i][file_id].append(float(row[i])) if row[i] != 'nan' 
-                 else datas[i][file_id].append(last_row[i]) for i in range(len(first_line)) if i < len(row)-1]
+                 else datas[i][file_id].append(last_row[i]) for i in range(len(first_line)) if i < len(row)]
                 last_row = row
         file_id += 1
 
@@ -43,7 +43,7 @@ def inverse_data(data: list):
 def plot_quality(q_mins: list, qualities: list, deltas: dict, deltas_rejected: dict, title: list = 'Title'):
     rows = int(np.ceil(len(q_mins)/2))
     cols = 2 if len(q_mins) > 1 else 1
-    fig, ax = plt.subplots(rows, cols, figsize=(15,15))
+    fig, ax = plt.subplots(rows, cols, figsize=(10,8))
     fig.suptitle(title, fontsize=16)
 
     for i,q_min in enumerate(q_mins):
@@ -52,21 +52,20 @@ def plot_quality(q_mins: list, qualities: list, deltas: dict, deltas_rejected: d
         if rows > 1:
             id_row = int(i / 2)
             id_col = i % cols
-            quality_ax(ax[id_row,id_col], x, y, -q_min)
             twin = ax[id_row,id_col].twinx()
             plot_deltas(twin, deltas, deltas_rejected)
+            quality_ax(ax[id_row,id_col], x, y, -q_min)
         elif cols > 1:
-            quality_ax(ax[i], x, y, -q_min)
             twin = ax[i].twinx()
             plot_deltas(twin, deltas, deltas_rejected)
+            quality_ax(ax[i], x, y, -q_min)
         else:
-            quality_ax(ax, x, y, -q_min)
             twin = ax.twinx()
             plot_deltas(twin, deltas, deltas_rejected)
+            quality_ax(ax, x, y, -q_min)
 
 
 def quality_ax(ax: plt.Axes, x, y ,q_min):
-    step = 0.5
     ax.plot(x,y)
     ax.set_title(f'Quality min={q_min}')
     ax.text(-1.5,q_min + 0.1, f'{q_min}', color='r')
@@ -85,33 +84,37 @@ def bar_chart(data, ax: plt.Axes, index = 0, color= '#0000ff'):
     x = np.arange(len(data))
     colors = color
 
-    copy = np.array([y if y > -100000 else 100000 for y in data])
+    copy = np.array([max(min(x, 100000), -100000) for x in data])
 
     mask_norm = copy > -100000
     mask_inf  = copy <= -100000
 
-    ax.bar(x[mask_norm] + offset,copy[mask_norm], width, color = colors)
+    ax.bar(x[mask_norm] + offset,copy[mask_norm], width, color = colors, alpha=0.2)
     # ax.bar(x[mask_inf] + offset ,copy[mask_inf], width, color = colors[1])
     ax.set_yscale('symlog')
 
 def plot_deltas(ax: plt.Axes, deltas: dict, deltas_rejected: dict):
-    bar_chart(deltas['Topological pass'][0], ax, 0, 'red')
-    bar_chart(deltas['Contraction pass'][0], ax, 1, 'blue')
-    bar_chart(deltas['Insertion pass'][0], ax, 2, 'green')
-    bar_chart(deltas['Smoothing pass'][0], ax, 3, 'orange')
-    bar_chart(deltas_rejected['Topological pass'][0], ax, 0, 'red')
-    bar_chart(deltas_rejected['Contraction pass'][0], ax, 1, 'blue')
-    bar_chart(deltas_rejected['Insertion pass'][0], ax, 2, 'green')
-    bar_chart(deltas_rejected['Smoothing pass'][0], ax, 3, 'orange')
+    i = 0
+    colors = ['red','blue','green','orange']
+    for types in deltas.keys():
+        for delta in deltas[types]:
+            bar_chart(delta,ax,i,colors[i])
+        i += 1
 
-def time_angle_experiment(q_min: float = 40):
-    filename = f"angle180_q_min{q_min}.csv"
+    i = 0
+    for types in deltas_rejected.keys():
+        for delta in deltas_rejected[types]:
+            bar_chart(delta,ax,i,colors[i])
+        i += 1
+
+
+def time_angle_experiment(turn:float=180, q_min: float = 40):
+    filename = f"angle{turn}_q_min{q_min}.csv"
     data,names = extract_data([filename])
     angles      = [x for x in data[0][0]]
     times       = [x/1000 for x in data[1][0]]
     quality     = [x for x in data[2][0]]
     quality_avg = [x for x in data[3][0]]
-    print(angles,times,quality,quality_avg)
     fig, ax = plt.subplots(figsize=(10,8))
     fig.subplots_adjust(right=0.75)
     fig.suptitle(f'Angle to time with q_min={q_min}')
@@ -130,10 +133,10 @@ def time_angle_experiment(q_min: float = 40):
 
     widths = np.pad(np.diff(angles), (0,1), mode="edge")
 
-    p1 = ax.bar(angles,times, color=base_color, edgecolor=f'dark{base_color}', width=widths, align='edge')
-    p2, = twin1.plot(angles, quality, color=quality_color, label="Worst element")
+    ax.bar(angles,times, color=base_color, edgecolor=f'dark{base_color}', width=widths, align='edge')
+    twin1.plot(angles, quality, color=quality_color, label="Worst element")
     twin1.axhline(y=float(q_min), color=quality_color, alpha=0.5, linestyle='dashed', label='Minimal quality')
-    p3, = twin2.plot(angles, quality_avg, color=average_color, label="Mesh average quality")
+    twin2.plot(angles, quality_avg, color=average_color, label="Mesh average quality")
     twin2.spines.right.set_position(("axes", 1.2))
 
     lines,labels = twin1.get_legend_handles_labels()
@@ -145,6 +148,8 @@ def quality_experiment():
     regex = re.compile(f'({filename}.*\.csv$)')
     files = find_files(regex)
     data, names = extract_data(files)
+    data = data[:-1]
+    names = names[:-1]
     pairs = zip(names,data)
     label_data = dict((name,value) for name, value in pairs)
 
@@ -182,11 +187,49 @@ def quality_experiment():
     plot_quality(label_data['Quality_min'], q_vectors_avg, deltas, deltas_rejected, "Average mesh quality")
 
 
+def turn_experiment(q_min:float = 50):
+    filename = f"turn_q_min{q_min}.csv"
+    data,names = extract_data([filename])
+    turn      = [x for x in data[0][0]]
+    times       = [x/1000 for x in data[1][0]]
+    quality     = [x for x in data[2][0]]
+    quality_avg = [x for x in data[3][0]]
+
+    fig, ax = plt.subplots(figsize=(10,8))
+    fig.subplots_adjust(right=0.75)
+    fig.suptitle(f'Time to turn with q_min={q_min}')
+
+    ax.set_xticks(np.arange(0.25, 1.25, 0.25))
+    ax.set_yscale('log')
+    ax.set_ylabel('Time (s)')
+
+    quality_color = 'red'
+    average_color='green'
+    base_color = 'blue'
+
+    twin1 = ax.twinx()
+    twin1.tick_params(axis='y', colors=quality_color)
+    twin2 = ax.twinx()
+    twin2.tick_params(axis='y', colors=average_color)
+    twin2.set_ylabel('Deformation energies')
+
+    ax.bar(turn,times, color=base_color, edgecolor=f'dark{base_color}', width=0.125)
+    twin1.plot(turn, quality, color=quality_color, label="Worst element")
+    twin1.axhline(y=float(q_min), color=quality_color, alpha=0.5, linestyle='dashed', label='Minimal quality')
+    twin2.plot(turn, quality_avg, color=average_color, label="Mesh average quality")
+    twin2.spines.right.set_position(("axes", 1.2))
+
+    lines,labels = twin1.get_legend_handles_labels()
+    lines2,labels2 = twin2.get_legend_handles_labels()
+    ax.legend(lines+lines2, labels+labels2, loc='upper left')
+
 
 def main():
     # time_angle_experiment(40)
-    # time_angle_experiment(35)
+    # time_angle_experiment(50)
+    # time_angle_experiment(360,80)
     quality_experiment()
+    # turn_experiment()
     plt.show()
     
 
